56c18c2ab23457324c4eca1593e5a1e318d807f1
BernardoFOFG <bernardofofg@gmail.com>
2024-11-26 20:52:50 -0300

    fix: header in layout

--- src/app.tsx
+++ src/app.tsx
@@ -2,8 +2,11 @@ import { Header } from './components/header';
 
 export const App = () => {
 	return (
-		<main className="max-w-6xl mx-auto flex flex-col py-16">
+		<main className=" flex flex-col py-8">
 			<Header />
+			<div className="max-w-6xl mx-auto">
+				<div className="h-[calc(100vh-156px)] bg-purple-500">A</div>
+			</div>
 		</main>
 	);
 };
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,28 +2,33 @@ import { LINK_NAVIGATE } from '@/lib/constants';
 
 export const Header = () => {
 	return (
-		<header className="flex justify-between items-center">
-			<div className="">
-				<img src="/elements/logo.svg" alt="logo" />
-			</div>
+		<header className="flex max-h-64 h-full backdrop-blur-sm bg-white py-4 sticky top-4">
+			<div className="flex w-full justify-between items-center max-w-7xl mx-auto">
+				<div className="">
+					<img src="/elements/logo.svg" alt="logo" />
+				</div>
 
-			<nav>
-				<ul className="flex items-center gap-8 font-semibold">
-					{LINK_NAVIGATE.map((link) => (
-						<li key={link.href}>
-							<a href={link.href} className="hover:text-primary transition-all">
-								{link.label}
-							</a>
-						</li>
-					))}
-					<a
-						href="/"
-						className="px-3 py-1 size-full border-2 border-white rounded-xl hover:bg-primary hover:border-transparent transition-all"
-					>
-						Login
-					</a>
-				</ul>
-			</nav>
+				<nav>
+					<ul className="flex items-center gap-8 font-semibold">
+						{LINK_NAVIGATE.map((link) => (
+							<li key={link.href}>
+								<a
+									href={link.href}
+									className="hover:text-primary transition-all"
+								>
+									{link.label}
+								</a>
+							</li>
+						))}
+						<a
+							href="/"
+							className="px-3 py-1 size-full border-2 border-white rounded-xl hover:bg-primary hover:border-transparent transition-all"
+						>
+							Login
+						</a>
+					</ul>
+				</nav>
+			</div>
 		</header>
 	);
 };
