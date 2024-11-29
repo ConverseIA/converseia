d79aae5350f590dc852261ca2db4315ff22fa10c
BernardoFOFG <bernardofofg@gmail.com>
2024-11-29 11:45:53 -0300

    feat: alterações pedidas pelo cliente 7.0

--- src/components/plans.tsx
+++ src/components/plans.tsx
@@ -1,4 +1,5 @@
-import { PLANS_AVALIABLE } from '@/lib/constants';
+import { LINK_CTA_WHATSAPP, PLANS_AVALIABLE } from '@/lib/constants';
+import { Button } from './ui/button';
 
 export const Plans = () => {
 	return (
@@ -8,33 +9,41 @@ export const Plans = () => {
 				você:
 			</h1>
 
-			<div className="grid base:grid-cols-1 md:grid-cols-3 base:gap-2 md:gap-6">
-				{PLANS_AVALIABLE.map((plans) => (
-					<div
-						className="border-2 border-background bg-gradient-to-b from-primary-light to-primary-dark p-6 flex flex-col gap-3 rounded-xl size-full"
-						key={plans.title}
-					>
-						<h3 className="text-center font-bold text-4xl">{plans.title}</h3>
-						<h4 className="text-center text-2xl font-semibold">
-							R$ {plans.price}/mês
-						</h4>
-						<div className="font-semibold">
-							{plans.list?.map((charge: any, index: any) => (
-								<div
-									key={`${plans.title}-${index}`}
-									className="flex items-start gap-2"
-								>
-									<img
-										src="/icons/verify-white.svg"
-										alt="verify-icon"
-										className="pt-2"
-									/>
-									<span>{charge}</span>
-								</div>
-							))}
+			<div className="flex flex-col gap-4">
+				<div className="grid base:grid-cols-1 md:grid-cols-3 base:gap-2 md:gap-6">
+					{PLANS_AVALIABLE.map((plans) => (
+						<div
+							className="border-2 border-background bg-gradient-to-b from-primary-light to-primary-dark p-6 flex flex-col gap-3 rounded-xl size-full"
+							key={plans.title}
+						>
+							<h3 className="text-center font-bold text-4xl">{plans.title}</h3>
+							<h4 className="text-center text-2xl font-semibold">
+								R$ {plans.price}/mês
+							</h4>
+							<div className="font-semibold">
+								{plans.list?.map((charge: any, index: any) => (
+									<div
+										key={`${plans.title}-${index}`}
+										className="flex items-start gap-2"
+									>
+										<img
+											src="/icons/verify-white.svg"
+											alt="verify-icon"
+											className="pt-2"
+										/>
+										<span>{charge}</span>
+									</div>
+								))}
+							</div>
 						</div>
-					</div>
-				))}
+					))}
+				</div>
+
+				<Button className="max-w-64 h-12 w-full mx-auto" asChild>
+					<a href={LINK_CTA_WHATSAPP} target="_blank" rel="noreferrer">
+						Teste grátis 20 dias
+					</a>
+				</Button>
 			</div>
 		</div>
 	);
