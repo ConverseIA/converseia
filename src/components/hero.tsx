60e1a0d93f096e40821d1faefc73b5924ef9d7dd
ConverseIA <contato@converseia.com>
2024-12-15 11:02:27 -0300

    novonumwpp

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -6,14 +6,14 @@ export const Hero = () => {
 		<div className="px-4 flex base:flex-col-reverse md:flex-row">
 			<div className="flex flex-col gap-4 justify-center base:items-center md:items-start base:text-center md:text-left">
 				<h1 className="max-w-xl text-4xl font-bold ">
-					'Automotize o atendimento com funcionários de IA que trabalham'{' '}
-					<span className="text-primary">24h por dia</span>
+					Automotize o atendimento com funcionários de IA que trabalham{' '}
+					<span className="text-primary">24h por dia.</span>
 				</h1>
 
 				<h3 className="max-w-sm">
-					'Agentes de Inteligência Artificial para receber toda a demanda de  
-					atendimento do seu escritório'
-					</h3>
+					Agentes de Inteligência Artificial para receber toda a demanda de
+					atendimento do seu escritório
+				</h3>
 				<Button
 					className="base:max-w-[65%] w-full md:max-w-56 base:h-16 h-12 mt-4 !px-2"
 					asChild
