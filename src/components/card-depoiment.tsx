31bfab55a2d2fd334ad8218fc7c65ffc6c38e1eb
ConverseIA <contato@converseia.com>
2025-04-11 15:11:23 -0300

    teste

--- src/components/card-depoiment.tsx
+++ src/components/card-depoiment.tsx
@@ -1,29 +1,27 @@
-import { getInitials } from '@/lib/constants';
-import { Avatar, AvatarFallback } from './ui/avatar';
+import { Avatar } from './ui/avatar';
 
 interface ICardDepoiment {
-	name: string;
-	job: string;
-	depoiment: string;
+  name: string;
+  job: string;
+  depoiment: string;
+  icon: string; // Propriedade para o ícone
 }
 
-export const CardDepoiment = ({ name, job, depoiment }: ICardDepoiment) => {
-	return (
-		<div className="bg-gradient-to-b from-primary-light to-primary-dark p-0.5 rounded-xl">
-			<div className="flex flex-col gap-4 bg-gradient-to-b from-dark-primary to-dark-secondary rounded-xl size-full p-6 min-h-40">
-				<div className="flex gap-2 items-center">
-					<Avatar>
-						<AvatarFallback className="bg-gradient-to-b from-primary-light to-primary-dark font-bold shadow">
-							{getInitials(name)}
-						</AvatarFallback>
-					</Avatar>
-					<span className="text-xl font-bold">
-						{name} - {job}
-					</span>
-				</div>
+export const CardDepoiment = ({ job, depoiment, icon }: ICardDepoiment) => {
+  return (
+    <div className="bg-gradient-to-b from-primary-light to-primary-dark p-0.5 rounded-xl">
+      <div className="flex flex-col gap-4 bg-gradient-to-b from-dark-primary to-dark-secondary rounded-xl size-full p-6 min-h-40">
+        <div className="flex gap-2 items-center">
+          <Avatar>
+            <img src={icon} alt={`Icon`} className="w-8 h-8" /> {/* Apenas o ícone */}
+          </Avatar>
+          <span className="text-xl font-bold">
+            {job}
+          </span>
+        </div>
 
-				<div className="text-white text-xl font-light">{`"${depoiment}"`}</div>
-			</div>
-		</div>
-	);
+        <div className="text-white text-xl font-light">{depoiment}</div>
+      </div>
+    </div>
+  );
 };
